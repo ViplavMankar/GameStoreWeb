3e6c5a29e3800041b3d89ebca9cff2e9c5b0a4f5
ViplavMankar <viplavmankar@gmail.com>
2025-03-27 17:17:15 +0530

    Changes to deploy app to Render

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    var port = Environment.GetEnvironmentVariable("PORT") ?? "5013";
+    app.Urls.Add($"http://0.0.0.0:{port}");
 }
 
 app.UseHttpsRedirection();
