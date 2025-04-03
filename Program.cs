bb928b1e155d0d0de9fd6cc2658e8db70dcd5f56
ViplavMankar <viplavmankar@gmail.com>
2025-04-03 11:58:45 +0530

    Changes related to Program.cs order of operations

--- Program.cs
+++ Program.cs
@@ -3,6 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+if (Environment.GetEnvironmentVariable("RENDER") != null) // Only on Render
+{
+    var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+    builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+}
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -11,13 +17,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    // app.UseHttpsRedirection();
 }
-else if (Environment.GetEnvironmentVariable("RENDER") != null) // Only on Render
+if (Environment.GetEnvironmentVariable("RENDER") != null) // Only on Render
 {
-    var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-    builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+    app.MapGet("/health", () => Results.Ok("API is running"));
 }
-
 // app.UseHttpsRedirection();
 app.UseStaticFiles();
 
