fb58340fca23962264fdb02229ccc8cb24c87758
ViplavMankar <viplavmankar@gmail.com>
2025-06-14 19:43:41 +0530

    Add Game option added through the UI

---
+++ Models/GameCreateDto.cs
@@ -0,0 +1,9 @@
+namespace GameStoreWeb.Models;
+
+public class GameCreateDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ThumbnailUrl { get; set; }
+    public string GameUrl { get; set; }
+}
