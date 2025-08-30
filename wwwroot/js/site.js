b5973e95b170ecaad765bfc924c48906030e7f26
ViplavMankar <viplavmankar@gmail.com>
2025-08-31 00:09:20 +0530

    Changes for S3 file upload and sessions implemented

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -6,3 +6,21 @@ window.getCookie = function (cookieName) {
     const match = document.cookie.match(new RegExp('(^| )' + cookieName + '=([^;]+)'));
     return match ? match[2] : null;
 };
+window.registerEndSession = function (sessionId) {
+    console.log("sessionId " + sessionId);
+    if (!sessionId) return;
+
+    window.addEventListener("beforeunload", function () {
+        // const ok = navigator.sendBeacon(
+        //     "https://localhost:7054/api/sessions/end-session",
+        //     new Blob([JSON.stringify({ SessionId: sessionId })], { type: "application/json" })
+        // );
+        // console.log("Beacon queued:", ok);
+        fetch(`${apiBaseUrl}/api/sessions/end-session`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ SessionId: sessionId }),
+            keepalive: true
+          });
+    });
+};
