e70525774f2460906193980d12b9bee33ed6519c
ViplavMankar <viplavmankar@gmail.com>
2026-04-24 19:13:27 +0530

    Dashboard View implementation as well as versioning changes

---
+++ Models/DashboardViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace GameStoreWeb.Models;
+
+public class DashboardViewModel
+{
+    public int TotalXP { get; set; }
+    public int CurrentStreak { get; set; }
+    public int MaxStreak { get; set; }
+    // public int Level { get; set; }
+    // public int XPForNextLevel { get; set; }
+    // public int GamesPlayed { get; set; }
+    // public int AchievementsUnlocked { get; set; }
+}
